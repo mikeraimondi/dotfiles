c234235bacf1d0e25ec91f3fe4e74a2903b8c519
Michael Raimondi <zeno@pobox.com>
2013-03-18 09:30:55 -0700

    Initial implementation of dependency install script.

---
+++ vim/installdependencies.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+############################
+# .make.sh
+# This script installs dependencies for vim and plugins
+############################
+
+# Make sure only root can run our script
+if [[ $EUID -ne 0 ]]; then
+   echo "This script must be run as root" 1>&2
+   exit 1
+fi
+
+# install apt packages
+echo "Installing packages"
+aptitude -y install git mercurial tmux ncurses-dev python-dev python-pip cmake
+echo "..done"
+
+# build Vim
+echo "Building Vim"
+mkdir /tmp/src
+cd /tmp/src
+hg clone https://vim.googlecode.com/hg/ vim
+cd /tmp/src/vim
+./configure --with-features=huge --enable-pythoninterp --with-python-config-dir=/usr/lib/python2.7/config
+make && make install
