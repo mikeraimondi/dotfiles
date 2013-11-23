2de1f505237036914f29d83a3e548ba9783d1673
Michael Raimondi <zeno@pobox.com>
2013-11-23 16:24:24 -0500

    Add Brewfile for brew bundle

---
+++ Brewfile
@@ -0,0 +1,11 @@
+install apple-gcc42
+install autoconf
+install automake
+install curl
+install git
+install gnupg
+install imagemagick
+install jsl
+install node
+install openssl
+install phantomjs
--- sublime_text_3/Packages/User/linters/ruby.py
+++ sublime_text_3/Packages/User/linters/ruby.py
@@ -2,5 +2,5 @@ from lint import Linter
 
 class Ruby(Linter):
     language = 'ruby'
-    cmd = ('/Users/mike/.rvm/bin/rvm-auto-ruby', '-wc')
+    cmd = ('ruby', '-wc')
     regex = r'^.+:(?P<line>\d+):\s+(?P<error>.+)'
