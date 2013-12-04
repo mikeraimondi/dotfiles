9461a27c401e95495e1474d2c67bb093706ef9db
Michael Raimondi <zeno@pobox.com>
2013-12-03 19:16:11 -0500

    Do not display battery status in liquidprompt

--- makesymlinks.sh
+++ makesymlinks.sh
@@ -8,7 +8,7 @@
 
 dir=~/dotfiles                    # dotfiles directory
 olddir=~/dotfiles_old             # old dotfiles backup directory
-files="bashrc bash_profile vimrc vim tmux.conf gitignore_global gitconfig config inputrc git-completion.bash"    # list of files/folders to symlink in homedir
+files="bashrc bash_profile vimrc vim tmux.conf gitignore_global gitconfig config inputrc git-completion.bash liquidpromptrc"    # list of files/folders to symlink in homedir
 
 ##########
 
