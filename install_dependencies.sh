adf760573db3f6e01908a66e4e7c0ef99b0ea411
Michael Raimondi <mike@mikeraimondi.com>
2015-02-19 20:11:22 -0500

    Add dependency installation script

---
+++ install_dependencies.sh
@@ -0,0 +1,2 @@
+apt-get update
+apt-get install autoconf bison build-essential libssl-dev libyaml-dev libreadline6-dev zlib1g-dev libncurses5-dev libffi-dev libgdbm3 libgdbm-dev python-dev cmake curl vim
