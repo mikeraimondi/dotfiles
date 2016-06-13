fa2a70cf795b887b15ac81bb9bc1a5d31a534172
Michael Raimondi <mike@mikeraimondi.com>
2016-06-13 16:27:20 -0400

    GPG, autosuggest, auto-VPN

--- hammerspoon/wifi.lua
+++ hammerspoon/wifi.lua
@@ -1,6 +1,7 @@
 -- WiFi triggers
 
 local homeSSID = 'strato-n.wifis.org'
+local vpnName  = 'streisand-direct'
 
 local speakers = hs.audiodevice.findOutputByName('Built-in Output')
 
@@ -8,6 +9,7 @@ hs.wifi.watcher.new(function()
   local previousSSID = hs.settings.get('previousSSID')
   local newSSID = hs.wifi.currentNetwork()
   local previouslyMuted = hs.settings.get('previouslyMuted')
+  local previouslyPublic = hs.settings.get('previouslyPublic')
 
   if newSSID == homeSSID and previousSSID ~= homeSSID then
     -- We just joined our home WiFi network
@@ -16,6 +18,19 @@ hs.wifi.watcher.new(function()
     -- We just departed our home WiFi network
     hs.settings.set('previouslyMuted', speakers:muted())
     speakers:setMuted(true)
+  end
+
+  if newSSID and hs.wifi.interfaceDetails()['security'] == 'None' then
+    hs.settings.set('previouslyPublic', true)
+    -- Wait for network to be joined
+    hs.timer.doAfter(5, function()
+      hs.osascript.applescript('tell application "Viscosity" to connect "' .. vpnName .. '"')
+    end):start()
+  else
+    hs.settings.set('previouslyPublic', false)
+    if previouslyPublic then
+      hs.osascript.applescript('tell application "Viscosity" to disconnect "' .. vpnName .. '"')
+    end
   end
 
   hs.settings.set('previousSSID', newSSID)
