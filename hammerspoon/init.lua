40d290bf21138d2d9bdb308201598543f20e8163
Michael Raimondi <mike@mikeraimondi.com>
2016-09-25 15:30:32 -0400

    Updates for VSCode

--- hammerspoon/init.lua
+++ hammerspoon/init.lua
@@ -1,6 +1,5 @@
 require('wifi')
 require('wm')
-require('net_switch')
 hs.timer.doEvery(hs.timer.days(1), function()
   hs.timer.doAfter(30, function()
     hs.reload() -- Reload config to workaround bug where hammerspoon watchers don't fire
