f2aa5c967b47c463c2ad2539364069e078bcb833
Michael Raimondi <mike@mikeraimondi.com>
2022-04-15 21:52:56 -0400

    Fresh install updates
    Signed-off-by: Michael Raimondi <mike@mikeraimondi.com>

--- config/fish/config.fish
+++ config/fish/config.fish
@@ -22,8 +22,8 @@ alias kls="kubens"
 
 # fish-specific setup
 fish_vi_key_bindings
-set __fish_git_prompt_show_informative_status 'yes'
-set __fish_git_prompt_showcolorhints 'yes'
+set __fish_git_prompt_show_informative_status yes
+set __fish_git_prompt_showcolorhints yes
 
 # colorized grep
 set -x GREP_OPTIONS '--color=auto'
@@ -33,23 +33,23 @@ set -x LD_RUN_PATH /usr/local/lib
 
 # Preferred editor for local and remote sessions
 if set -q SSH_CONNECTION
-  set -x EDITOR 'vim'
+    set -x EDITOR vim
 else
-  set -x EDITOR $HOME/.editor
+    set -x EDITOR $HOME/.editor
 end
 set -x BUNDLER_EDITOR $EDITOR
 set -x GURNEL_EDITOR "code -w -n -a . $argv"
 
 # Java
 if test -e /usr/libexec/java_home
-	set -x JAVA_HOME (/usr/libexec/java_home)
+    set -x JAVA_HOME (/usr/libexec/java_home)
 else
-	set -x JAVA_HOME /usr/lib/jvm/default-java
+    set -x JAVA_HOME /usr/lib/jvm/default-java
 end
 
 # Go
 if test -d $HOME/go/bin/go
- set -x PATH $PATH $HOME/go/bin
+    set -x PATH $PATH $HOME/go/bin
 end
 set -x GOPATH $HOME/Documents/projects/go
 set -x PATH $PATH $GOPATH/bin
@@ -57,7 +57,7 @@ set -x PATH $PATH /usr/local/opt/go/libexec/bin
 
 # Rust
 if test -d $HOME/.cargo/bin
-  set -x PATH $PATH $HOME/.cargo/bin
+    set -x PATH $PATH $HOME/.cargo/bin
 end
 
 # Fisherman
@@ -67,16 +67,13 @@ if not functions -q fisher
     fish -c fisher
 end
 
-# Google Cloud SDK.
-source "/usr/local/Caskroom/google-cloud-sdk/latest/google-cloud-sdk/path.fish.inc"
-
 # misc
-set -g fish_user_paths "/usr/local/opt/curl/bin" $fish_user_paths
+set -g fish_user_paths /usr/local/opt/curl/bin $fish_user_paths
 # TODO figure out how to make "j" work with https://github.com/fish-shell/fish-shell/blob/72d80c3d91bbd35bed0aafb5514c9834bb48e256/share/completions/j.fish
 # set -U Z_CMD "j"
-set -U FISH_KUBECTL_COMPLETION_TIMEOUT "2s"
-set -g fish_user_paths "/usr/local/opt/ruby/bin" $fish_user_paths
+set -U FISH_KUBECTL_COMPLETION_TIMEOUT 2s
+set -g fish_user_paths /usr/local/opt/ruby/bin $fish_user_paths
 direnv hook fish | source
 if test -d "$HOME/Library/Application Support/Code/User/globalStorage/ms-vscode-remote.remote-containers/cli-bin"
-	set -x PATH $PATH "$HOME/Library/Application Support/Code/User/globalStorage/ms-vscode-remote.remote-containers/cli-bin"
+    set -x PATH $PATH "$HOME/Library/Application Support/Code/User/globalStorage/ms-vscode-remote.remote-containers/cli-bin"
 end
